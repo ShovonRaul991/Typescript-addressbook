74f6eb1b3a86a0fbe2ce8936bc5a6a39794bec8e
Shovon Raul <shovonraul18@gmail.com>
2023-02-03 14:01:06 +0530

    submission

--- page.js
+++ page.js
@@ -8,9 +8,18 @@ var personWebsite = document.getElementById("WebsiteEntry");
 var personAddress = document.getElementById("AddressEntry");
 var inputform = document.getElementById("input-form-hidden");
 var addressview = document.getElementById("view-details");
-var form = (document.getElementById('form-id'));
+var form = document.getElementById('form-id');
 var inputs = document.getElementsByTagName('input');
 var contactlist = document.getElementById('contact-list-items');
+var nameHeader = document.getElementById('Person-Name');
+var viewDetailsBody = document.getElementById("view-details-body");
+var detailEmail = document.getElementById('detail-email-value');
+var detailMobile = document.getElementById('detail-mobile-value');
+var detailLandline = document.getElementById('detail-landline-value');
+var detailWebSite = document.getElementById('detail-site-value');
+var detailAddress = document.getElementById('detail-address-value');
+var human1 = document.getElementById('1');
+var buttons = document.getElementsByClassName('contact-list-item');
 var person = /** @class */ (function () {
     function person(name, email, mobile, landline, website, address) {
         this.name = name;
@@ -23,6 +32,37 @@ var person = /** @class */ (function () {
     return person;
 }());
 var Identity = []; //empty array to store all the objects
+var person1 = new person('Chandermani Arora', 'Chandermani@technovert.com', 9192929292922, 2334567890, 'www.technovert.com', 'abc streat, some road, madhapur, hyderabad-500033');
+var person2 = new person('Sash Pagadala', 'Chandermani@technovert.com', 9192923452922, 233456789, 'www.keka.com', 'abc streat, some road, rajpur, hyderabad-500133');
+Identity = [person1, person2];
+nameHeader.innerHTML = person1.name;
+detailEmail.innerHTML = person1.email;
+detailMobile.innerHTML = String(person1.mobile);
+detailLandline.innerHTML = String(person1.landline);
+detailAddress.innerHTML = person1.address;
+detailWebSite.innerHTML = person1.website;
+function creatingContact() {
+    var count = 1;
+    Identity.forEach(function (human) {
+        var contact = document.createElement('div');
+        contact.setAttribute('class', 'contact-list-item');
+        contact.setAttribute('id', String(count));
+        var nameValue = document.createElement('p');
+        nameValue.setAttribute('class', 'name');
+        nameValue.innerHTML = human.name;
+        var emailValue = document.createElement('p');
+        emailValue.setAttribute('class', 'email');
+        emailValue.innerHTML = human.email;
+        var mobileValue = document.createElement('p');
+        mobileValue.setAttribute('class', 'mobile');
+        mobileValue.innerHTML = String(human.mobile);
+        contact.appendChild(nameValue);
+        contact.appendChild(emailValue);
+        contact.appendChild(mobileValue);
+        contactlist.appendChild(contact);
+        count++;
+    });
+}
 function openInputForm() {
     addressview.style.display = 'none';
     inputform.style.display = 'block';
@@ -33,6 +73,7 @@ function addDetails() {
         /* creating the list */
         var contact = document.createElement('div');
         contact.setAttribute('class', 'contact-list-item');
+        contact.setAttribute('id', 'human' + Identity.length);
         var nameValue = document.createElement('p');
         nameValue.setAttribute('class', 'name');
         nameValue.innerHTML = personName.value;
@@ -47,16 +88,29 @@ function addDetails() {
         contact.appendChild(mobileValue);
         contactlist.appendChild(contact);
     }
+    //creatingContact();
     inputform.style.display = 'none';
-    addressview.style.display = 'block';
+    //addressview.style.display = 'block';
     form.reset();
+}
+var _loop_1 = function (i) {
+    buttons[i].addEventListener('click', function () {
+        var index = Number(buttons[i].id);
+        //console.log(Identity[index-1].name);
+        nameHeader.innerHTML = Identity[index - 1].name;
+        detailEmail.innerHTML = Identity[index - 1].email;
+        detailMobile.innerHTML = String(Identity[index - 1].mobile);
+        detailLandline.innerHTML = String(Identity[index - 1].landline);
+        detailAddress.innerHTML = Identity[index - 1].address;
+        detailWebSite.innerHTML = Identity[index - 1].website;
+    }, false);
+};
+for (var i = 0; i < buttons.length; i++) {
+    _loop_1(i);
 }
 function goBacktoHome() {
     inputform.style.display = 'none';
     addressview.style.display = 'block';
+    human1.style.backgroundColor = '#CEE7F2';
     form.reset();
-}
-function errorFilled() {
-    if (personName.value == "")
-        return false;
 }
--- page.ts
+++ page.ts
@@ -1,16 +1,25 @@
 let addNav = document.querySelector('.AddAddress');
 let addButton = document.querySelector('#submitButton') as HTMLInputElement;
-let personName = (document.getElementById("NameEntry") as HTMLInputElement);
-let personEmail = (document.getElementById("EmailEntry") as HTMLInputElement);
-let personMobile = (document.getElementById('MobileEntry') as HTMLInputElement);
-let personLandline = (document.getElementById("LandlineEntry") as HTMLInputElement);
-let personWebsite = (document.getElementById("WebsiteEntry") as HTMLInputElement);
-let personAddress = (document.getElementById("AddressEntry") as HTMLInputElement);
-let inputform = (document.getElementById("input-form-hidden") as HTMLElement);
-let addressview = (document.getElementById("view-details") as HTMLElement);
-let form = (document.getElementById('form-id')) as HTMLFormElement;
+let personName = document.getElementById("NameEntry") as HTMLInputElement;
+let personEmail = document.getElementById("EmailEntry") as HTMLInputElement;
+let personMobile = document.getElementById('MobileEntry') as HTMLInputElement;
+let personLandline = document.getElementById("LandlineEntry") as HTMLInputElement;
+let personWebsite = document.getElementById("WebsiteEntry") as HTMLInputElement;
+let personAddress = document.getElementById("AddressEntry") as HTMLInputElement;
+let inputform = document.getElementById("input-form-hidden") as HTMLElement;
+let addressview = document.getElementById("view-details") as HTMLElement;
+let form = document.getElementById('form-id') as HTMLFormElement;
 let inputs = document.getElementsByTagName('input');
 let contactlist = document.getElementById('contact-list-items') as HTMLElement;
+let nameHeader = document.getElementById('Person-Name') as HTMLElement;
+let viewDetailsBody = document.getElementById("view-details-body") as HTMLElement;
+let detailEmail = document.getElementById('detail-email-value') as HTMLSpanElement
+let detailMobile = document.getElementById('detail-mobile-value') as HTMLSpanElement;
+let detailLandline = document.getElementById('detail-landline-value') as HTMLSpanElement;
+let detailWebSite = document.getElementById('detail-site-value') as HTMLSpanElement;
+let detailAddress = document.getElementById('detail-address-value') as HTMLSpanElement;
+let human1 = document.getElementById('1') as HTMLElement;
+let buttons  = document.getElementsByClassName('contact-list-item');
 
 class person{
     name:string;
@@ -30,6 +39,46 @@ class person{
 }
 
 let Identity : person[] = []; //empty array to store all the objects
+const person1 = new person('Chandermani Arora','Chandermani@technovert.com',9192929292922,2334567890,'www.technovert.com','abc streat, some road, madhapur, hyderabad-500033');
+const person2 = new person('Sash Pagadala','Chandermani@technovert.com',9192923452922,233456789,'www.keka.com','abc streat, some road, rajpur, hyderabad-500133');
+Identity = [person1,person2];
+
+
+nameHeader.innerHTML = person1.name;
+detailEmail.innerHTML = person1.email;
+detailMobile.innerHTML = String(person1.mobile);
+detailLandline.innerHTML = String(person1.landline);
+detailAddress.innerHTML = person1.address;
+detailWebSite.innerHTML = person1.website;
+
+function creatingContact(){
+    let count = 1;
+    Identity.forEach(human=>{
+        const contact = document.createElement('div');
+        contact.setAttribute('class', 'contact-list-item');
+        contact.setAttribute('id',String(count)) 
+
+        const nameValue = document.createElement('p')
+        nameValue.setAttribute('class','name')
+        nameValue.innerHTML = human.name;
+        
+        const emailValue = document.createElement('p');
+        emailValue.setAttribute('class','email');
+        emailValue.innerHTML = human.email;
+
+        const mobileValue = document.createElement('p')
+        mobileValue.setAttribute('class','mobile');
+        mobileValue.innerHTML = String(human.mobile);
+        
+        contact.appendChild(nameValue);
+        contact.appendChild(emailValue)
+        contact.appendChild(mobileValue);
+
+        contactlist.appendChild(contact);
+        count++;
+
+    })
+}
 
 function openInputForm(){
     addressview.style.display = 'none';
@@ -45,6 +94,7 @@ function addDetails(){
 
         const contact = document.createElement('div');
         contact.setAttribute('class', 'contact-list-item');
+        contact.setAttribute('id','human'+Identity.length);
 
         const nameValue = document.createElement('p')
         nameValue.setAttribute('class','name')
@@ -63,25 +113,40 @@ function addDetails(){
         contact.appendChild(mobileValue);
 
         contactlist.appendChild(contact);
+        
     }
 
-
+    //creatingContact();
     inputform.style.display = 'none';
-    addressview.style.display = 'block';
+    //addressview.style.display = 'block';
     form.reset();
 }
 
-function selectContact(){
-    
-}
+
+
+
+
+
+    for(let i =0;i<buttons.length;i++){
+        buttons[i].addEventListener('click',function(){
+            let index = Number(buttons[i].id);
+            //console.log(Identity[index-1].name);
+            nameHeader.innerHTML = Identity[index-1].name;
+            detailEmail.innerHTML = Identity[index-1].email;
+            detailMobile.innerHTML = String(Identity[index-1].mobile);
+            detailLandline.innerHTML = String(Identity[index-1].landline);
+            detailAddress.innerHTML = Identity[index-1].address;
+            detailWebSite.innerHTML = Identity[index-1].website;
+        }, false);
+    }
+
+
+
 
 function goBacktoHome(){
     inputform.style.display = 'none';
     addressview.style.display = 'block';
+    human1.style.backgroundColor = '#CEE7F2';
     form.reset();
-}
-
-function errorFilled(){
-    if(personName.value =="")
-     return false;
+    
 }
